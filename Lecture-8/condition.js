728fbe4bf2ce0d228d68bd6e2cfe4aff11481ecc
Kushal-Saxena999 <saxenakushal924@gmail.com>
2024-11-27 03:39:40 +0530

    Adding Lectures to JS Foundations

---
+++ Lecture-8/condition.js
@@ -0,0 +1,75 @@
+//Checking if a number is greater than another number:
+
+// let num1 = 5;
+// let num2 = 8;
+// console.log("I am regular upper code");
+
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// }
+
+// console.log("I am regular bottom code");
+
+
+
+// let num1 = 5;
+// let num2 = 8;
+// console.log("I am regular upper code");
+
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// } else{
+//   console.log("Nope, num1 is not greater");
+// }
+
+// console.log("I am regular bottom code");
+
+
+
+
+//Checking if a string is equal to another string:
+
+// let username = "Tea";
+// let anotherUsername = "Tea";
+
+// if (username == anotherUsername) {//not use assignment operator
+//   console.log("Pick another username");
+// }else {
+//   console.log("You can pick this username");
+// }
+
+
+//Checking if a variable is a number or not:
+
+/*
+let score = 44;//"44"
+
+if (typeof score === 'number') {
+  console.log("Yep, this is  a number");
+} else {
+  console.log("No that is not a number");
+}
+
+*/
+
+//Checking if a variable is true or false:
+
+let isTeaReady = false;
+
+if (isTeaReady){
+  console.log("Tea is ready");
+} else {
+  console.log("Tea is not ready");
+}
+
+//Checking if an array is Empty or not:
+
+let items = ["item1"];
+
+console.log("items.length");
+
+if (items.length === 0) {
+  console.log("The array is empty");
+}else {
+  console.log("The array is not empty");
+}
