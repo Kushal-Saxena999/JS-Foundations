e94c6ba405c35563a58bd7935c2ee9614fa8c408
Kushal-Saxena999 <saxenakushal924@gmail.com>
2024-11-26 12:01:07 +0530

    Adding Lectures to JS Fundamentals

---
+++ Lecture-3/datatypes.js
@@ -0,0 +1,34 @@
+
+console.log("hello");
+//console.log("Tea");
+
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102
+
+let score = 102;
+let name = "chaicode.com"
+let isLoggedin = false
+
+//object
+
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"]
+let user = {firstname: "kushal", lastname: "Saxena"}
+
+let getScore = score;
+
+console.log(getScore);
