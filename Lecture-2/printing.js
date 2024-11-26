8615c7b6014000a7b4628ac9d23034ebc786ba1f
Kushal-Saxena999 <saxenakushal924@gmail.com>
2024-11-26 10:29:33 +0530

    Adding Lectures to Js Fundamentals

---
+++ Lecture-2/printing.js
@@ -0,0 +1,11 @@
+
+
+//Ways of Printing data
+console.log("Hello");
+console.log("chai");
+process.stdout.write("tea");
+process.stdout.write("tea");
+
+
+console.table({city: "Rampur"});
+console.warn({city: "UP"});
