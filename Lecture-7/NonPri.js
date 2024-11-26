24c39dec1e4ae609d562605ee47b79b55f35891b
Kushal-Saxena999 <saxenakushal924@gmail.com>
2024-11-26 21:31:35 +0530

    Adding Lectures for JS Foundation

---
+++ Lecture-7/NonPri.js
@@ -0,0 +1,39 @@
+let username = {
+  firstname: "Kushal",
+  isLoggedin: true,
+};
+
+username.firstName = "Ritik";
+username.lastname = "Saxena";
+
+
+// console.log(username.firstname);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(username["firstname"]);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+//Array
+
+// let heros = ["a", "b", "c", true]
+
+let anotherUser = ["kushal", true];
+
+// console.log(anotherUser[0]);
+
+// console.log(1 + '1');
+
+// let isValue = true;
+// console.log(isValue + 1); 
+
+// let isValue = "2";
+// console.log(Number(isValue));
+
+let isValue = "2abc";
+console.log(Number(isValue));
+console.log(typeof Number(isValue));
+console.log(Number(null));
+console.log(Number(undefined));
